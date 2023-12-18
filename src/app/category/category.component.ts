1c7cf1edf945f1100997c851faaedb48843960f7
Ximo Llacer <xllaber7@gmail.com>
2023-12-18 18:34:23 +0100

    added html and logic to category

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Category} from "../models/category";
+import {CATEGORIES} from "../models/CATEGORIES";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-category',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent {
+
+  categories: Category[] = CATEGORIES;
+  category: any = {};
+  // @Input() id!:number;
+
+  constructor(private activeRoute: ActivatedRoute) {
+    this.activeRoute.params.subscribe(params => {
+      this.category = this.getCategory(params["id"] - 1);
+      console.log(this.category);
+    })
+  }
+
+  getCategory(id: number) {
+    return this.categories[id];
+  }
 
 }
