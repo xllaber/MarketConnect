7ceec57643e0f0107544a19e6c52b37cefa99d82
Ximo Llacer <xllaber7@gmail.com>
2023-12-18 18:34:32 +0100

    refactor styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Category} from "../models/category";
 import {CATEGORIES} from "../models/CATEGORIES";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,13 @@ export class HomeComponent {
 
   heroBanner: string = "assets/img/hero-banner.png";
   categories: Category[] = CATEGORIES;
+  @Output() categorySelect!: EventEmitter<number>;
+
+  constructor(private router: Router) {}
+
+  showCategory(id: number) {
+    console.log("JOLA");
+    this.router.navigate(["/category", id]);
+  }
 
 }
